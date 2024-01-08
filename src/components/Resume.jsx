c17536f9fc0adaa534e4d3e1ae61f7ae9c15585c
Kevinpg123 <osmarkevinp@gmail.com>
2024-01-07 22:31:18 -0300

    nuevos cambios

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,4 +1,4 @@
-import resume from "../assets/images/currifculum.png";
+import resume from "../assets/images/resume.png";
 import { useTheme } from "./ThemeContext";
 
 export default function Skills() {
@@ -8,13 +8,18 @@ export default function Skills() {
     <section id="resume" className="resume-section">
       <h2 className={`page-header page-header-${theme}`}>Resume</h2>
       <div className="resume__container">
-
-        <img
-          className={`resume__img${theme === "light" ? " light" : ""}`}
-          src={resume}
-          title="Click to view PDF"
-        />
-
+        <a
+          href="https://res.cloudinary.com/ddx1ikpma/image/upload/v1703889203/resume_xwxwn7.png"
+          className="resume__link"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img
+            className={`resume__img${theme === "light" ? " light" : ""}`}
+            src={resume}
+            title="Click to view PDF"
+          />
+        </a>
       </div>
     </section>
   );
