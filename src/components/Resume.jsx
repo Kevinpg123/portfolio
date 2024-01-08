5b4e9c2d92540ac806ae5165a6e7b368ee198f2f
Kevinpg123 <osmarkevinp@gmail.com>
2024-01-07 22:05:18 -0300

    version 1.1

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -8,18 +8,13 @@ export default function Skills() {
     <section id="resume" className="resume-section">
       <h2 className={`page-header page-header-${theme}`}>Resume</h2>
       <div className="resume__container">
-        <a
-          href="https://res.cloudinary.com/ddx1ikpma/image/upload/v1703889203/resume_xwxwn7.png"
-          className="resume__link"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img
-            className={`resume__img${theme === "light" ? " light" : ""}`}
-            src={resume}
-            title="Click to view PDF"
-          />
-        </a>
+
+        <img
+          className={`resume__img${theme === "light" ? " light" : ""}`}
+          src={resume}
+          title="Click to view PDF"
+        />
+
       </div>
     </section>
   );
