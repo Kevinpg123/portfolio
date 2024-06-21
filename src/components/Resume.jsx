8a9694cc1e795cbc923cd5cb6abbddbd06edd7aa
Kevin Osmar Pauluk <osmarkevinp@gmail.com>
2024-06-21 00:02:46 -0300

    estilos cv

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,6 +1,7 @@
 import resume from "../assets/images/cvnuevo1.png";
 import resume2 from "../assets/images/cvnuevo2.png"
 import { useTheme } from "./ThemeContext";
+import style from './Resume.module.css'
 
 export default function Skills() {
   const { theme } = useTheme();
@@ -9,20 +10,15 @@ export default function Skills() {
     <section id="resume" className="resume-section">
       <h2 className={`page-header page-header-${theme}`}>Resume</h2>
       <div style={{ marginLeft: '1em' }} className="resume__container">
-        <a
-          href="https://drive.google.com/file/d/1pJLPM-mY_tj-0ypwroIWz_kZ0dith0mK/view?usp=sharing"
-          className="resume__link"
-          rel="noreferrer"
-          target="_blank"
-        >
-          <img
 
-            className={`resume__img${theme === "light" ? " light" : ""}`}
-            src={resume}
-            title="Click to view PDF"
-          />
+        <img
 
-        </a>
+          className={`resume__img${theme === "light" ? " light" : ""}`}
+          src={resume}
+          title="Click to view PDF"
+        />
+
+
 
         <p>Read More: </p>
         <a
@@ -30,7 +26,7 @@ export default function Skills() {
           className="resume__link"
           rel="noreferrer"
           target="_blank"
-        ><img src="https://firebasestorage.googleapis.com/v0/b/portfolio-aeb2a.appspot.com/o/bandera%20inglesa.png?alt=media&token=cfee7fff-dfff-4c81-b42d-a54860ae2144"></img>
+        ><img className={style.flagImg} src="https://firebasestorage.googleapis.com/v0/b/portfolio-aeb2a.appspot.com/o/bandera%20inglesa.png?alt=media&token=cfee7fff-dfff-4c81-b42d-a54860ae2144"></img>
         </a>
         <a
           href="https://drive.google.com/file/d/1R8PMVDJEtnvXbx88-nGk0YSuwzlVv8zW/view?usp=sharing"
@@ -38,7 +34,7 @@ export default function Skills() {
           rel="noreferrer"
           target="_blank"
         >
-          <img src="https://firebasestorage.googleapis.com/v0/b/portfolio-aeb2a.appspot.com/o/Bandera_de_Espa%C3%B1a_(sin_escudo).svg.png?alt=media&token=12ea8528-5f95-4890-9142-e39509639395"></img>
+          <img className={style.flagImg} src="https://firebasestorage.googleapis.com/v0/b/portfolio-aeb2a.appspot.com/o/Bandera_de_Espa%C3%B1a_(sin_escudo).svg.png?alt=media&token=12ea8528-5f95-4890-9142-e39509639395"></img>
         </a>
       </div>
 
